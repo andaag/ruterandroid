537b608f19eaf31a4be03d815fa5ed07c59313aa
Anders Aagaard <aagaande@gmail.com>
2009-11-21 11:15:59 +0100

    adding selectDeviTask for selecting a devi and opening it in a browser

---
+++ src/com/neuron/trafikanten/tasks/SelectDeviTask.java
@@ -0,0 +1,88 @@
+/**
+ *     Copyright (C) 2009 Anders Aagaard <aagaande@gmail.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.neuron.trafikanten.tasks;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+import com.neuron.trafikanten.dataSets.DeviData;
+
+/*
+ * This class takes a devi data set, and returns nothing.
+ */
+
+public class SelectDeviTask implements GenericTask {
+    private static final String TAG = "Trafikanten-SelectDeviTask";
+    private Activity activity;
+    private ArrayList<DeviData> devi;
+    
+    public SelectDeviTask(Activity activity, ArrayList<DeviData> devi) 
+    {
+        this.activity = activity;
+        this.devi = devi;
+        showDialog();
+    }
+    
+    /*
+     * Setup dialog for selecting devi
+     */
+    private void showDialog() {
+        /*
+         * Setup list of devi titles
+         */
+        ArrayList<String> deviList = new ArrayList<String>();
+        for (DeviData deviData : devi) {
+        	deviList.add(deviData.title);
+        }
+        
+        /*
+         * Setup select devi dialog
+         */
+        final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        //builder.setTitle("Select Devi");
+        final String[] items = new String[deviList.size()];
+        deviList.toArray(items);
+        
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                    dialog.dismiss();
+                    deviSelected(devi.get(item));
+            }
+        });
+        
+        AlertDialog dialog = builder.create();
+        
+        /*
+         * Show dialog
+         */
+        dialog.show();
+    }
+    
+    private void deviSelected(DeviData deviData) {
+
+    }
+
+	@Override
+	public void stop() {
+		// No background data to stop.
+	}
+}
