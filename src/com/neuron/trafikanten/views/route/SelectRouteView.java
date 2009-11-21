4f767fa1c0b7a962cb94ea6f98143579e87434d3
Anders Aagaard <aagaande@gmail.com>
2009-11-21 20:36:21 +0100

    redesign of select route view

--- src/com/neuron/trafikanten/views/route/SelectRouteView.java
+++ src/com/neuron/trafikanten/views/route/SelectRouteView.java
@@ -24,32 +24,55 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
-import android.app.Activity;
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+import android.widget.LinearLayout.LayoutParams;
 
 import com.neuron.trafikanten.R;
 import com.neuron.trafikanten.dataSets.RouteData;
 import com.neuron.trafikanten.dataSets.StationData;
+import com.neuron.trafikanten.views.route.SelectRouteAdapter.CheckboxRouteEntry;
+import com.neuron.trafikanten.views.route.SelectRouteAdapter.GenericRouteEntry;
+import com.neuron.trafikanten.views.route.SelectRouteAdapter.SeperatorRouteEntry;
+import com.neuron.trafikanten.views.route.SelectRouteAdapter.SimpleTextRouteEntry;
 
-public class SelectRouteView extends Activity {
+public class SelectRouteView extends ListActivity {
 	private static final String TAG = "Trafikanten-SelectRouteView";
 	private static final int ACTIVITY_SELECT_FROM = 1;
 	private static final int ACTIVITY_SELECT_TO = 2;
 	
-	private ViewHolder viewHolder = new ViewHolder();
+	private static final int DIALOG_SELECTTIME = 1;
+	
+	private SelectRouteAdapter listMenu;
+	
+	/*
+	 * Saved instance data:
+	 */
+	private boolean advancedOptionsVisible = false;
 	private RouteData routeData = new RouteData();
+	private boolean preferDirect = false;
+	private boolean travelAt = true; // if false it's arriveBefore
 	
 	/*
 	 * Options menu items:
@@ -61,105 +84,126 @@ public class SelectRouteView extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.selectroute);
-
-		/*
-		 * Setup viewHolder
-		 */
-		viewHolder.fromButton = (Button) findViewById(R.id.fromButton);
-		viewHolder.toButton = (Button) findViewById(R.id.toButton);
-		viewHolder.travelAtArriveBeforeButton = (Button) findViewById(R.id.travelAtArriveBefore);
-		viewHolder.timePicker = (TimePicker) findViewById(R.id.timePicker);
-		viewHolder.departureDay = (Spinner) findViewById(R.id.departureDay);
-		viewHolder.changeMargin = (Spinner) findViewById(R.id.changeMargin);
-		viewHolder.searchButton = (Button) findViewById(R.id.searchButton);
 		
-		/*
-		 * Setup time picker:
-		 */
-        {
-            viewHolder.timePicker.setIs24HourView(true);
-            final Calendar calendar = Calendar.getInstance();
-            viewHolder.timePicker.setCurrentHour(calendar.get(Calendar.HOUR_OF_DAY));
-            viewHolder.timePicker.setCurrentMinute(calendar.get(Calendar.MINUTE));
-        }
-        
-        /*
-         * Setup travel at/arrive before button
-         */
-        viewHolder.travelAtArriveBeforeButton.setTag(Boolean.TRUE);
-        {
-        	viewHolder.travelAtArriveBeforeButton.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					final Boolean status = (Boolean) viewHolder.travelAtArriveBeforeButton.getTag();
-					viewHolder.travelAtArriveBeforeButton.setTag(!status);
-					if (status) {
-						viewHolder.travelAtArriveBeforeButton.setText(R.string.arriveBefore);
-					} else {
-						viewHolder.travelAtArriveBeforeButton.setText(R.string.travelAt);
-					}
-															
-				}
-        		
-        	});
-        }
-        
-        /*
-         * Setup change margin list:
-         */
-        {
-			ArrayAdapter<String> changeMarginAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, 
-					new String[] {"0","1","2","3","4","5","10"});
-			changeMarginAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-			viewHolder.changeMargin.setAdapter(changeMarginAdapter);
-			viewHolder.changeMargin.setSelection(2);
-        }
+		listMenu = new SelectRouteAdapter();
 		
 		/*
 		 * Load instance state
 		 */
 		if (savedInstanceState != null) {
-			routeData = savedInstanceState.getParcelable(RouteData.PARCELABLE);
+			
 		}
+		refreshMenu();
+		setListAdapter(listMenu);
+	}
+	
+	private void resetView() {
+		routeData = new RouteData();
+		preferDirect = false;
+		advancedOptionsVisible = false;
+		refreshMenu();
+	}
+	
+	/*
+	 * This renders the entire menu
+	 */
+	public void refreshMenu() {
+		ArrayList<GenericRouteEntry> items = new ArrayList<GenericRouteEntry>();
+		/*
+		 * Setup the basic menu:
+		 */
+		items.add(new SeperatorRouteEntry(this, "Departure and arrival"));
 		
 		/*
-		 * Fill list of days and refresh button list
+		 * Setup From
 		 */
-		fillDayList();
-		refreshButtons();
-		
-		/*
-		 * Setup fromButton
-		 */
-		viewHolder.fromButton.setOnClickListener(new OnClickListener() {
+		items.add(new SimpleTextRouteEntry(this, "From","Select where to travel from", 0, new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(SelectRouteView.this, SelectRouteStationView.class);
 				startActivityForResult(intent, ACTIVITY_SELECT_FROM);
+				refreshMenu();
 			}
-		});
+		}));
 		
 		/*
-		 * Setup toButton
+		 * Setup to
 		 */
-		viewHolder.toButton.setOnClickListener(new OnClickListener() {
+		items.add(new SimpleTextRouteEntry(this, "To","Select your destination", 0, new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(SelectRouteView.this, SelectRouteStationView.class);
 				startActivityForResult(intent, ACTIVITY_SELECT_TO);
+				refreshMenu();
 			}
-		});
+		}));
 		
+		items.add(new SeperatorRouteEntry(this, "Settings"));
 		/*
-		 * Setup searchButton
+		 * Setup when
 		 */
-		viewHolder.searchButton.setOnClickListener(new OnClickListener() {
+		final SimpleDateFormat DATEFORMAT = new SimpleDateFormat("EEEEEEE dd-MM-yyyy HH:mm");
+		final String travelTime = routeData.departure == 0 ? "Now" : DATEFORMAT.format(routeData.departure);
+		items.add(new SimpleTextRouteEntry(this, "When","Travel at : " + travelTime, 5, new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				search();
+				SelectRouteView.this.showDialog(DIALOG_SELECTTIME);
+				refreshMenu();
 			}
-		});
+		}));
+		
+		
+		{
+			/*
+			 * advanced onClickListener is shared
+			 */
+			final OnClickListener advancedOnClickListener = new OnClickListener() {
+	
+				@Override
+				public void onClick(View v) {
+					advancedOptionsVisible = !advancedOptionsVisible;
+					refreshMenu();
+				}
+				
+			};
+			
+			if (advancedOptionsVisible) {
+				items.add(new SimpleTextRouteEntry(this, "Advanced","Hide advanced options", 0, advancedOnClickListener));
+				
+				
+				
+				/*
+				 * Add advanced options
+				 */
+				items.add(new CheckboxRouteEntry(this, "Prefer direct",true, 10, new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						preferDirect = !preferDirect;
+						refreshMenu();
+					}
+				}));
+				items.add(new CheckboxRouteEntry(this, "Like monkeys",true, 10, new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						
+					}
+				}));
+			} else {
+				items.add(new SimpleTextRouteEntry(this, "Advanced","Show advanced options", 0, advancedOnClickListener));
+			}
+		}
+		
+		items.add(new SimpleTextRouteEntry(this, "Search","Search for routes", 0, new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				onSearch();
+			}
+			
+		}));
+		
+		listMenu.setList(items);
+		listMenu.notifyDataSetChanged();
 	}
 	
 	/*
@@ -174,20 +218,6 @@ public class SelectRouteView extends Activity {
 		final MenuItem help = menu.add(0, HELP_ID, 0, R.string.help);
 		help.setIcon(android.R.drawable.ic_menu_help);
 		return true;
-	}
-	
-	/*
-	 * Reset view (used by RESET_ID and show settings)
-	 */
-	private void resetView() {
-    	/*
-    	 * Reset view
-    	 */
-        viewHolder.timePicker.setCurrentHour(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
-        viewHolder.timePicker.setCurrentMinute(Calendar.getInstance().get(Calendar.MINUTE));
-		routeData = new RouteData();
-		fillDayList();
-		refreshButtons();		
 	}
 
 	/*
@@ -211,32 +241,9 @@ public class SelectRouteView extends Activity {
 	}
 	
 	/*
-	 * Refresh station information
-	 */
-	private void refreshButtons() {
-		/*
-		 * FromButton
-		 */
-		if (routeData.fromStation == null) {
-			viewHolder.fromButton.setText(R.string.travelFrom);
-		} else {
-			viewHolder.fromButton.setText(routeData.fromStation.stopName);			
-		}
-		
-		/*
-		 * ToButton
-		 */
-		if (routeData.toStation == null) {
-			viewHolder.toButton.setText(R.string.travelTo);
-		} else {
-			viewHolder.toButton.setText(routeData.toStation.stopName);			
-		}
-	}
-	
-	/*
 	 * Do actual search
 	 */
-	private void search() {
+	public void onSearch() {
 		if (routeData.fromStation == null) {
 			Toast.makeText(this, R.string.pleaseSelectFrom, Toast.LENGTH_SHORT).show();
 			return;
@@ -246,23 +253,15 @@ public class SelectRouteView extends Activity {
 			return;
 		}
 		
-
-        /*
-         * Convert TimePicker + departureDay holder to a (long) Date
-         */
-        try {
-                Date date = DATEFORMAT.parse((String) viewHolder.departureDay.getSelectedItem());
-                date.setHours(viewHolder.timePicker.getCurrentHour());
-                date.setMinutes(viewHolder.timePicker.getCurrentMinute());
-                routeData.departure = date.getTime();
-        } catch (ParseException e) {
-                // This should NEVER happen...
-                Log.e(TAG,"ParseException " + e);
-                Toast.makeText(this, "ParseException parsing day (this should NEVER happen, report!)", Toast.LENGTH_LONG).show();
-        }
-		
+		boolean departureNow = routeData.departure == 0;
+		if (departureNow) {
+			routeData.departure = Calendar.getInstance().getTimeInMillis();			
+		}
 		
 		OverviewRouteView.ShowRoute(this, routeData);
+		if (departureNow) {
+			routeData.departure = 0;
+		}
 	}
 	
 	/*
@@ -283,87 +282,228 @@ public class SelectRouteView extends Activity {
 			} else {
 				routeData.toStation = station;
 			}
-			refreshButtons();
 			break;
 		default:
 			Log.e(TAG, "Unknown request code " + requestCode);
 		}
-	}
-
-	/*
-	 * Setup list of days in "Select day to travel" dropdown.
-	 */
-	private final static SimpleDateFormat DATEFORMAT = new SimpleDateFormat("EEEEEEE dd-MM-yyyy");
-	private void fillDayList() {
-		ArrayList<String> dateList = new ArrayList<String>();
-		
-		Calendar date = Calendar.getInstance();
-		for (int i = 0; i < 14; i++) {
-			dateList.add(DATEFORMAT.format(date.getTime()));
-			date.add(Calendar.DAY_OF_YEAR, 1);
-		}
-		
-		ArrayAdapter<String> dayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, dateList);
-		dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		viewHolder.departureDay.setAdapter(dayAdapter);
+		refreshMenu();
 	}
 	
-	/*
-	 * onPause
-	 * @see android.app.Activity#onPause()
-	 */
+	
+	
 	@Override
-	protected void onPause() {
-		super.onPause();
+	protected Dialog onCreateDialog(int id) {
+		switch(id) {
+		case DIALOG_SELECTTIME:
+			/*
+			 * Create a dialog for selecting travel day and time.
+			 */
+			final Dialog dialog = new Dialog(this);
+			dialog.setContentView(R.layout.selectroute_dialog_timepick);
+			dialog.setTitle("Select travel time");
+
+			/*
+			 * Setup the travel at/arrive before spinner.
+			 */
+			final ArrayList<String> travelAtArriveBeforeItems = new ArrayList<String>();
+			travelAtArriveBeforeItems.add("Travel at");
+			travelAtArriveBeforeItems.add("Arrive before");
+			final Spinner travelAtArriveBeforeSpinner = (Spinner) dialog.findViewById(R.id.travelAtArriveBefore);
+			final ArrayAdapter<String> travelAtArriveBeforeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, travelAtArriveBeforeItems);
+			travelAtArriveBeforeSpinner.setAdapter(travelAtArriveBeforeAdapter);
+			
+			/*
+			 * Setup the time picker
+			 */
+			final TimePicker timePicker = (TimePicker) dialog.findViewById(R.id.timePicker);
+			timePicker.setIs24HourView(true);
+			
+			/*
+			 * Setup list of days in "Select day to travel" dropdown.
+			 */
+			final Spinner dayList = (Spinner) dialog.findViewById(R.id.dayList);
+			final SimpleDateFormat DATEFORMAT = new SimpleDateFormat("EEEEEEE dd-MM-yyyy");
+			ArrayList<String> dateList = new ArrayList<String>();
+			
+			Calendar date = Calendar.getInstance();
+			for (int i = 0; i < 14; i++) {
+				dateList.add(DATEFORMAT.format(date.getTime()));
+				date.add(Calendar.DAY_OF_YEAR, 1);
+			}
+			
+			final ArrayAdapter<String> dayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, dateList);
+			dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			dayList.setAdapter(dayAdapter);
+			
+			/*
+			 * Setup button
+			 */
+			final Button okButton = (Button) dialog.findViewById(R.id.okButton);
+			okButton.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					try {
+						travelAt = travelAtArriveBeforeSpinner.getSelectedItemPosition() == 0;
+						
+						Date date = DATEFORMAT.parse(dayAdapter.getItem(dayList.getSelectedItemPosition()));
+		                date.setHours(timePicker.getCurrentHour());
+		                date.setMinutes(timePicker.getCurrentMinute());
+		                routeData.departure = date.getTime();
+						refreshMenu();
+						dialog.dismiss();
+					} catch (ParseException e) {
+						// This can't happen.
+					}					
+				}
+				
+			});
+			
+			return dialog;
+		}
+		return super.onCreateDialog(id);
 	}
 
-	/*
-	 * Resume state, restart search.
-	 * @see android.app.Activity#onResume()
-	 */
 	@Override
-	protected void onResume() {
-		super.onResume();
-        /*
-         * Check current time, if it's too old, reset it.
-         */
-        Date date;
-        try {
-                date = DATEFORMAT.parse((String) viewHolder.departureDay.getSelectedItem());
-                date.setHours(viewHolder.timePicker.getCurrentHour());
-                date.setMinutes(viewHolder.timePicker.getCurrentMinute());
-                
-                Calendar currentDate = Calendar.getInstance();
-                currentDate.roll(Calendar.MINUTE, -30);
-                if (date.before(currentDate.getTime())) {
-                        /*
-                         * Date is timepicker is more than 30 minutes old, reset it.
-                         */
-                        viewHolder.timePicker.setCurrentHour(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
-                        viewHolder.timePicker.setCurrentMinute(Calendar.getInstance().get(Calendar.MINUTE));
-                }
+	protected void onPrepareDialog(int id, Dialog dialog) {
+		switch(id) {
+		case DIALOG_SELECTTIME:
+			/*
+			 * Simply recreate this dialog.
+			 */
+			removeDialog(DIALOG_SELECTTIME);
+			dialog = onCreateDialog(DIALOG_SELECTTIME);
+			break;
+		}
+		super.onPrepareDialog(id, dialog);
+	}
 
-        } catch (ParseException e) {
-                Log.e(TAG,"ParseException onResume, this should never happen " + e);
-        }
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		listMenu.click(position);
 	}
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
-		outState.putParcelable(RouteData.PARCELABLE, routeData);
-	}
-	
-	/*
-	 * Static class for holding the gui elements
-	 */
-	static class ViewHolder {
-		Button fromButton;
-		Button toButton;
-		Button travelAtArriveBeforeButton;
-		TimePicker timePicker;
-		Spinner departureDay;
-		Spinner changeMargin;
-		Button searchButton;
 	}
 }
+
+
+class SelectRouteAdapter extends BaseAdapter
+{
+	private ArrayList<GenericRouteEntry> items = new ArrayList<GenericRouteEntry>();
+	
+	public void setList(ArrayList<GenericRouteEntry> items) {
+		this.items = items;
+	}
+	
+	public void click(int pos) {
+		items.get(pos).onClick();
+	}
+	
+	@Override
+	public int getCount() {
+		return items.size();
+	}
+
+	@Override
+	public GenericRouteEntry getItem(int pos) {
+		return items.get(pos);
+	}
+
+	@Override
+	public long getItemId(int pos) {
+		return pos;
+	}
+
+	@Override
+	public View getView(int pos, View arg1, ViewGroup arg2) {
+		return items.get(pos).mView;
+	}
+	
+	@Override
+	public boolean isEnabled(int position) {
+		return !items.get(position).getClass().equals(SeperatorRouteEntry.class);
+	}
+
+
+
+
+	/*
+	 * Fairly complicated view to render everything in the select route list.
+	 *   - Supports Linearlayout with titles and extra text
+	 *   - Supports checkboxes
+	 *   - Supports expandable, and having a parent to see if it's been expanded.
+	 */
+	public static class GenericRouteEntry {
+        public View mView;
+        private OnClickListener onClickListener;
+        
+        public GenericRouteEntry(OnClickListener onClickListener) {
+        	this.onClickListener = onClickListener;
+        }
+        
+        public void onClick() {
+        	if (onClickListener != null)
+        		onClickListener.onClick(mView);
+        }
+    }
+	
+	public static class SeperatorRouteEntry extends GenericRouteEntry {
+		private TextView mText;
+		
+		public SeperatorRouteEntry(Context context, String text) {
+			super(null);
+			mText = new TextView(context);
+			mText.setText(text);
+			mText.setGravity(Gravity.CENTER);
+			mView = mText;
+			
+		}
+	}
+	
+	public static class SimpleTextRouteEntry extends GenericRouteEntry {
+        private TextView mTitle;
+        private TextView mSubText;
+        
+        /*
+         * This is a simple layout, a text with a smaller subtext.
+         */
+		public SimpleTextRouteEntry(Context context, String title, String extra, int paddingLeft, OnClickListener onClickListener) {
+			super(onClickListener);
+        	LinearLayout linearLayout = new LinearLayout(context);
+        	linearLayout.setPadding(paddingLeft, 0, 0, 0);
+        	linearLayout.setOrientation(LinearLayout.VERTICAL);
+        	
+            mTitle = new TextView(context);
+            mTitle.setText(title);
+            mTitle.setTextAppearance(context, android.R.style.TextAppearance_Medium);
+            linearLayout.addView(mTitle, new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+
+            mSubText = new TextView(context);
+            mSubText.setText(extra);
+            mSubText.setTextAppearance(context, android.R.style.TextAppearance_Small);
+            linearLayout.addView(mSubText, new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+            mView = linearLayout;
+        }
+	}
+	
+	public static class CheckboxRouteEntry extends GenericRouteEntry {
+        private CheckBox mCheckBox;
+		/*
+		 * This is a checkbox layout.
+		 */
+		public CheckboxRouteEntry(Context context, String title, boolean checked, int paddingLeft, OnClickListener onClickListener) {
+			super(onClickListener);
+			mCheckBox = new CheckBox(context);
+			mCheckBox.setPadding(paddingLeft, 0, 0, 0);
+			mCheckBox.setText(title);
+			mCheckBox.setChecked(checked);
+			mView = mCheckBox;
+		}
+	}
+	
+}
+
+
