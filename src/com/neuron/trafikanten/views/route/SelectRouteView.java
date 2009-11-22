536fa95bc0c69c8e3fedbf6472b76a4dbe81317e
Anders Aagaard <aagaande@gmail.com>
2009-11-22 12:47:51 +0100

    prettier travel at/arrive before

--- src/com/neuron/trafikanten/views/route/SelectRouteView.java
+++ src/com/neuron/trafikanten/views/route/SelectRouteView.java
@@ -354,6 +354,7 @@ public class SelectRouteView extends ListActivity {
 			travelAtArriveBeforeItems.add("Arrive before");
 			final Spinner travelAtArriveBeforeSpinner = (Spinner) dialog.findViewById(R.id.travelAtArriveBefore);
 			final ArrayAdapter<String> travelAtArriveBeforeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, travelAtArriveBeforeItems);
+			travelAtArriveBeforeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 			travelAtArriveBeforeSpinner.setAdapter(travelAtArriveBeforeAdapter);
 			
 			/*
