56eade42d2ad7a2f17882d25953778b64891914d
Anders Aagaard <aagaande@gmail.com>
2010-02-21 09:29:28 +0100

    forbedret showrealtimelinedetails

--- src/com/neuron/trafikanten/tasks/ShowRealtimeLineDetails.java
+++ src/com/neuron/trafikanten/tasks/ShowRealtimeLineDetails.java
@@ -25,29 +25,34 @@ public class ShowRealtimeLineDetails implements GenericTask {
         showDialog();
     }
     
+    private void addText(LinearLayout body, String text) {
+    	TextView infoLine = new TextView(activity);
+    	infoLine.setMovementMethod(ScrollingMovementMethod.getInstance());
+    	infoLine.setHorizontallyScrolling(true);
+    	infoLine.setSingleLine();
+    	infoLine.setText(text);
+    	body.addView(infoLine);
+
+    }
+    
     private void renderDeparture(LinearLayout body, long expectedDeparture, boolean realtime, String stopVisitNote) {
     	StringBuffer info = new StringBuffer();
+    	info.append(data.line + " " + data.destination + " ");
     	if (!realtime) {
     		info.append(activity.getText(R.string.ca));
     		info.append(" ");
     	}
     	info.append(HelperFunctions.renderTime(activity, expectedDeparture));
+    	addText(body, info.toString());
     	if (stopVisitNote != null) {
-    		info.append(" - " + stopVisitNote);
+    		addText(body, "      " + stopVisitNote);
     	}
-    	
-    	TextView infoLine = new TextView(activity);
-    	infoLine.setMovementMethod(ScrollingMovementMethod.getInstance());
-    	infoLine.setHorizontallyScrolling(true);
-    	infoLine.setSingleLine();
-    	infoLine.setText(info);
-    	body.addView(infoLine);
     }
     
     private void showDialog() {
 		//dialog = new Dialog(activity, android.R.style.Theme);
     	dialog = new Dialog(activity);
-    	dialog.setTitle(data.line + " " + data.destination);
+    	dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		dialog.setContentView(R.layout.dialog_showrealtimeline);
 		
 		final LinearLayout body = (LinearLayout) dialog.findViewById(R.id.body); 
@@ -55,23 +60,6 @@ public class ShowRealtimeLineDetails implements GenericTask {
 		for (NextDeparture departure : data.nextDepartures) {
 			renderDeparture(body, departure.expectedDeparture, departure.realtime, departure.stopVisitNote);
 		}
-		
-		/*final TextView validPeriod = (TextView) dialog.findViewById(R.id.validPeriod);
-		final TextView title = (TextView) dialog.findViewById(R.id.title);
-		final TextView description = (TextView) dialog.findViewById(R.id.description);
-		final TextView body = (TextView) dialog.findViewById(R.id.devibody);
-		
-		validPeriod.setText(activity.getText(R.string.validFrom) + " " + dateFormater.format(data.validFrom).toString());
-		title.setText(stripCode(data.title));
-		description.setText(stripCode(data.description));
-		
-		final CharSequence bodyText = stripCode(data.body);
-		if (bodyText.length() < 3) {
-			body.setVisibility(View.GONE);
-		}
-		else {
-			body.setText(stripCode(data.body));
-		}*/
 		
 		dialog.show();
     }
