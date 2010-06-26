8661682409528c7bbc4a4e1da9cc72fff554faea
Anders Aagaard <aagaande@gmail.com>
2010-06-26 19:17:36 +0200

    marking 2.0.7-TEST2

--- src/com/neuron/trafikanten/tasks/SelectContactTask.java
+++ src/com/neuron/trafikanten/tasks/SelectContactTask.java
@@ -27,7 +27,6 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
-import android.content.DialogInterface.OnClickListener;
 import android.database.Cursor;
 import android.location.Address;
 import android.location.Geocoder;
