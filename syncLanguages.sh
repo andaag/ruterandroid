0f1e0ab44d3462d001f8069480493a0b53946584
Anders Aagaard <aagaande@gmail.com>
2011-08-14 13:45:31 +0200

    sync languages between region codes

--- syncLanguages.sh
+++ syncLanguages.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 fixlang() {
+    echo "fixlang $1"
     rm -rf res/values-$1
-    cp res/values-no res/values-$1
+    cp -r res/values-nb res/values-$1
 }
 fixlang no
+fixlang sv
 
 
 
 fixlangRegion() {
+    echo "fixlang $1 region $2"
     rm -rf res/values-$1-r$2
-    cp res/values-no res/values-$1-r$2
+    cp -r res/values-nb res/values-$1-r$2
 }
 fixlangRegion en NO
